9073a64ad3a2c072b49b74fef6344bbe6ca04936
suckitfa <254202042@qq.com>
2022-07-15 08:16:33 +0800

    feat: apply middleware in app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// 引入校验客户端
+var checkAPP = require('./utils/middleware')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,9 +13,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// 静态服务器
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+// 使用中间件,本项目中所有自定义路由都是用该中间件
+app.use('/',checkAPP ,indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
