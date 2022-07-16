173e009ce663a69be679e48ea67e1e7e9b901a0e
Bob Tang <254202042@qq.com>
2022-07-16 10:45:47 +0800

    feat: time format

--- utils/common.js
+++ utils/common.js
@@ -6,4 +6,9 @@ util.getReturnData = (code, message = '', data = []) => {
     } 
     return {code,message,data};
 }
+// 格式化时间
+util.getLocalDate = (t) => {
+    let date = new Date(parseFloat(t));
+    return date.getFullYear() + '-' + (parseInt(date.getMonth()) + 1) + '-' + date.getDate() + " " + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+}
 module.exports = util;
